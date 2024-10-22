cb369dca2ecf912ef227fa9abd190409748815e9
Yave-Balderas <id21136@dasc.edu.mx>
2024-10-22 01:38:21 -0600

    Se añadió Carrito

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 //import './Componetes/login.js';
 import { Productos } from './Componetes/Productos';
 import { Login } from './Componetes/Login';
+import { Carrito } from './Componetes/Carrito';
 
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Carrito/>
     </div>
     
   );
